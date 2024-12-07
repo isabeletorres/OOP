960dac03579106c03206e7b6f1f4f246e5681e77
Isabele Torres <isabeledasilvatorres@gmail.com>
2024-12-07 12:18:45 -0300

    Adicinando o .gitignore, adiconando a classe triangulo

---
+++ ParteUm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+namespace ParteUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            x.A = (10);
+            x.B = (11);
+            x.C = (12);
+
+            double areaX = x.Area();
+
+            y.A = (12);
+            y.B = (13);
+            y.C = (14);
+
+            double areaY= y.Area();
+
+            Console.WriteLine($"Área de X = {areaX.ToString("F4", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Área de Y = {areaY.ToString("F4", CultureInfo.InvariantCulture)}");
+        }   
+    }
+}
