22437d822b2fc58127d1b656808e8acbf973cf9a
Isabele Torres <isabeledasilvatorres@gmail.com>
2024-12-21 10:33:37 -0300

    Adicionando um pequeno projeto com composição, enumeradores e entidades

---
+++ Composição/Entities/Departament.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao.Entities
+{
+    internal class Departament
+    {
+        public String Name { get; set; }
+
+        public Departament()
+        {
+        }
+        public Departament(String name)
+        {
+            Name = name;
+        }
+    }
+}
