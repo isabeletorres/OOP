f4ffacb55ab3fca71c12707d37d6a69e0333194e
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-01-01 20:26:39 -0300

    Add herança

---
+++ Heranca/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Heranca.Entities;
+
+namespace Heranca
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brouw", 100, 500);
+            Console.WriteLine(account.Balance);
+            Console.ReadKey();
+        }
+    }
+}
