88b46a748d1baf1092189507645f48080cacfc3a
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-01-01 20:54:53 -0300

    UpCasting and DownCasting

--- Heranca/Program.cs
+++ Heranca/Program.cs
@@ -8,8 +8,34 @@ namespace Heranca
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brouw", 100, 500);
-            Console.WriteLine(account.Balance);
+            Account acc = new Account(1001, "Alex", 0.0);
+
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500);
+
+            //UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 500);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.00);
+
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
             Console.ReadKey();
         }
     }
