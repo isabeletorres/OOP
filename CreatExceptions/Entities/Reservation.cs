32abaa4810c3b6b5efaf4f692244eaa075c6e0cb
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-02-02 21:42:23 -0300

    Adicionando a primeira parte do exercício de fixação de exeções

---
+++ CreatExceptions/Entities/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatExceptions.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", Check-In: "
+                + CheckIn.ToString("dd/mm/yyyy")
+                + ", Check-Out: "
+                + CheckOut.ToString("dd/mm/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ CreatExceptions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CreatExceptions.Entities;
+
+namespace CreatExceptions
+{
+    public class ProcessFile
+    {
+        public static void Main()
+        {
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/mm/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/mm/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            Reservation resevartion = new Reservation(number, checkIn, checkOut);
+
+
+            
+        }
+    }
+}
