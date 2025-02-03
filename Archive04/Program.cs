ab2fd0587c75ba47bc8a78c98088a355bbbc40e9
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-02-03 18:45:52 -0300

    Adiconando mais exercicios sobre manipulação de arquivos

---
+++ Archive04/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Archive04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\temp\myfolder";
+            
+
+            try
+            {
+
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\pastaMinha");
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
