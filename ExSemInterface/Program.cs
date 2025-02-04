1f16d6119d30149562b4780785e5a19d9394d73a
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-02-04 13:24:45 -0300

    Terminando o exercicio sem interface

--- ExSemInterface/Program.cs
+++ ExSemInterface/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using ExSemInterface.Entities;
+using ExSemInterface.Services;
 
 namespace ExSemInterface
 {
@@ -11,20 +12,24 @@ namespace ExSemInterface
             Console.WriteLine("Enter rental data:");
             Console.Write("Car model: ");
             string model = Console.ReadLine();
-            Console.Write("Pickup (dd/mm/yyy hh:mm): ");
-            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyy HH:mm", CultureInfo.InvariantCulture);
-            Console.Write("Return (dd/mm/yyy hh:mm): ");
-            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine());
+            Console.Write("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine());
 
             CarRental carRental = new CarRental(start, finish, new Vehicle(model));
 
-            Console.Write("Enter price per hour: ");
-            Console.Write("Enter price per day: ");
-            Console.WriteLine("INVOICE: ");
-            Console.Write("Basic payment: ");
-            Console.Write("Tax: ");
-            Console.Write("Total payment: ");
+            RentalService rentalService = new RentalService(hour, day);
 
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoce");
+            Console.WriteLine(carRental.Invoice);
         }
     }
 }
