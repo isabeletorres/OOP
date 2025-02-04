f85496e95f4f841c65e7d0c2fbeb0eb17bd5952c
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-02-04 12:31:02 -0300

    Adicionando uma parte de um exercício sem o uso de uma inteface ainda

---
+++ ExSemInterface/Entities/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExSemInterface.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double basicPayment, double tax)
+        {
+            BasicPayment = basicPayment;
+            Tax = tax;
+        }
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+        public override string ToString()
+        {
+            return "Basic Payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                +"\n Tax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\n Total Payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ExSemInterface/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using ExSemInterface.Entities;
+
+namespace ExSemInterface
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data:");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/mm/yyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            Console.Write("Enter price per hour: ");
+            Console.Write("Enter price per day: ");
+            Console.WriteLine("INVOICE: ");
+            Console.Write("Basic payment: ");
+            Console.Write("Tax: ");
+            Console.Write("Total payment: ");
+
+        }
+    }
+}
